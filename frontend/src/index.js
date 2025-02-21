9bcb740831c3a4e32cd869bb6fac335ed7644842
Tekkx8 <cristalboy666@gmail.com>
2025-02-21 02:36:13 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const queryClient = new QueryClient();
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
